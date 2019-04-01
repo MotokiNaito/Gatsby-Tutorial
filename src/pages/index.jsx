a216534ba24ca74376c863f59ad09c97c87966f2
Motoki Naito <motoki.developer@gmail.com>
2019-04-01 06:56:04 -0700

    add transformer plugin

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,37 @@
 import React from "react"
-import Header from "../components/header"
-import styled from "styled-components"
 import { graphql } from "gatsby"
 
-const Container = styled.div`
-  max-width: 600px;
-  margin: 0 auto;
-`
-
-export default ({ data }) => (
-  <Container>
-    <Header />
-    {console.log(data)}
-  </Container>
-)
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <div>
+      <h1>Amazing Pandas Eating Things</h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export const query = graphql`
   query {
-    site {
-      siteMetadata {
-        title
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
       }
     }
   }
