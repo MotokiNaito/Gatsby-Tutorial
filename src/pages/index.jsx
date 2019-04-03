37ab76424d33e5bbd243ce3babb856e4ff073ee3
Motoki Naito <motoki.developer@gmail.com>
2019-04-03 07:48:40 -0700

    finished this tutorial

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,20 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 export default ({ data }) => {
-  console.log(data)
   return (
     <div>
       <h1>Amazing Pandas Eating Things</h1>
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <h3>
-            {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
-          </h3>
-          <p>{node.excerpt}</p>
+          <Link to={node.fields.slug}>
+            <h3>
+              {node.frontmatter.title}
+              <span>— {node.frontmatter.date}</span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </Link>
         </div>
       ))}
     </div>
@@ -21,7 +23,7 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -29,6 +31,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
